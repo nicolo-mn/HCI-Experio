66d11bb46ac30a9c60a755bae4b033d94154cdf8
Nicolò Monaldini <nicolo.monaldini@gmail.com>
2026-02-04 12:44:21 +0100

    fix: reload error when deployed

--- A5/experio/src/router.js
+++ A5/experio/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import SignUp from './components/SignUp.vue'
 import LeTueZone from './components/LeTueZone.vue'
 import Home from './components/Home.vue'
@@ -37,7 +37,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
 })
 
